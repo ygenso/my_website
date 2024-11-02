cc616a0ab4c3bba51a323226960cc57d9e67e7cf
ygenso <160308765+ygenso@users.noreply.github.com>
2024-11-03 03:03:19 +0900

    ikeru?

--- JS/prime.js
+++ JS/prime.js
@@ -1,25 +1,28 @@
 var myfunc = function(){
     alert()
-    let answer = false
+    let answer = 0
+    //1は素数、0は合成数
     let divi = []
     //取得
     let num = document.getElementById("inputs").value;
     //調べる
     if ( num == 1 ){
-        answer = false
+        answer = 0
         divi = [1] //一つなのでpushなし
     }else if ( num == 2 ){
-        answer = true
+        answer = 1
         divi = [2] //一つなのでpushなし
     }else{
         for ( i = 2; i == num; i++){
             if( num %  i == 0){
-                answer = false
+                answer = 0
                 divi.push(i)
             }
         }
+        if ( divi.length == 0){
+            answer = 1
+        }
     }
-    alert(YNan)
     //表示
     var mydiv = document.getElementById("YNan");
     mydiv.innerHTML = answer;
