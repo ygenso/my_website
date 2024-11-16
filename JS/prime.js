920270c8bfc697e89d4e0b207e88a4c82a8fcd4b
ygenso <160308765+ygenso@users.noreply.github.com>
2024-11-16 22:57:25 +0900

    prime

--- JS/prime.js
+++ JS/prime.js
@@ -3,6 +3,33 @@ var myfunc = function(){
     let num = document.getElementById("inputs").value;
     alert(num)
     //調べる
+    if (num <= 1){
+        return{
+            rumber: num,
+            isPrime: false,
+            isComposite: false,
+            divisors: [num],
+        };
+    }
+
+    let divisors = [];
+    let isPrime = true;
+
+    for (let i = 1; i <= num; i++){
+        if (num % i === 0){
+            divisors.push(i);
+            if (i !== 1 && i !== num) {
+                isPrime = false; //素数ではない
+            }
+        }
+    }
+
+    return {
+        number: num,
+        isPrime: isPrime,
+        isComposite: !isPrime,
+        divisors: divisors,
+    };
     //表示
     var mydiv = document.getElementById("YNan");
     mydiv.innerHTML = answer;
