b513ce0e5f30da779f95d78e1884e0b71dee2274
ygenso <160308765+ygenso@users.noreply.github.com>
2024-11-16 22:46:12 +0900

    prime

--- JS/prime.js
+++ JS/prime.js
@@ -1,32 +1,8 @@
 var myfunc = function(){
-    alert()
-    let answer = 0
-    //1は素数、0は合成数
-    let divi = []
     //取得
     let num = document.getElementById("inputs").value;
+    alert(num)
     //調べる
-    if ( num == 1 ){
-        answer = 0
-        divi = [1,2] //一つなのでpushなし
-    }else if ( num == 2 ){
-        answer = 1
-        divi = [2] //一つなのでpushなし
-    }else{
-        alert("divi")
-        for ( i = 2; i == num; i++){
-            alert("answer")
-            if( num %  i == 0){
-                answer = 0
-                divi.push(i)
-                
-            }
-        }
-        if ( divi.length == 1){
-            answer = 1
-            
-        }
-    }
     //表示
     var mydiv = document.getElementById("YNan");
     mydiv.innerHTML = answer;
