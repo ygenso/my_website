30442b78fcb839362ce9fbcee43a05e05bb9fda7
ygenso <160308765+ygenso@users.noreply.github.com>
2025-03-01 19:31:30 +0900

    修正

--- JS/oxidation.js
+++ JS/oxidation.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-//ページの読み込みが完了したら実行
-document.addEventListener('DOMContentLoaded', function() {
-    //ボタン要素を取得
-    const selected = document.getElementById('Cu_button');
-
-    //ボタンがクリックされた時のイベントリスナーを追加
-    selected.addEventListener('click', function() {
-        //セレクトボックスから選択された要素を取得
-        const selected_cuo = document.getElementById('Cu_select').value;
-        //選択された要素を表示
-        alert(selected_cuo);
-    }
-    );
-}
-);
-=======
 function CuOm() {
     alert();
     // ラジオボタンの選択値を取得
@@ -43,4 +26,3 @@ function CuOm() {
     }
 
 }
->>>>>>> 605cce124d75b42069f44eb0ea36d2ce78f20f51
