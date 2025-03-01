7b1ad6fa03682cf6f08da6af219405b6bc0e7dd1
ygenso <160308765+ygenso@users.noreply.github.com>
2025-03-01 17:38:48 +0900

    test

--- JS/oxidation.js
+++ JS/oxidation.js
@@ -1,33 +1,15 @@
-function CuOm() {
-    // ラジオボタンの選択値を取得
-    let elements = document.getElementsByName("Cu_select");
-    let len = elements.length;
-    let checkValue = "";
+//ページの読み込みが完了したら実行
+document.addEventListener('DOMContentLoaded', function() {
+    //ボタン要素を取得
+    const selected = document.getElementById('Cu_button');
 
-    for (let i = 0; i < len; i++) {
-        if (elements.item(i).checked) {
-            checkValue = elements.item(i).value;
-        }
+    //ボタンがクリックされた時のイベントリスナーを追加
+    selected.addEventListener('click', function() {
+        //セレクトボックスから選択された要素を取得
+        const selected_cuo = document.getElementById('Cu_select').value;
+        //選択された要素を表示
+        alert(selected_cuo);
     }
-    alert(checkValue);
-
-    // テキストボックスの値を取得
-    let CuO = document.getElementById("CuO").value;
-    let O = document.getElementById("O").value;
-    let Cu = document.getElementById("Cu").value;
-
-    // テキストボックスの値を数値に変換
-    CuO = Number(CuO);
-    O = Number(O);
-    Cu = Number(Cu);
-
-    if (checkValue === "CuO") {
-        let one = CuO / 5;
-        let O = one * 2;
-        let Cu = one * 3;
-        document.getElementById('CuOm').textContent = CuO;
-        document.getElementById('Om').textContent = O;
-        document.getElementById('Cum').textContent = Cu;
-    }
-
+    );
 }
+);
